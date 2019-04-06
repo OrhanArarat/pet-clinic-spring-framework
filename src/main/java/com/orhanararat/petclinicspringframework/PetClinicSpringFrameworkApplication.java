ecdf5be5827a8c61fdef1070c29d298cbfd7d93c
Orhan Ararat <orhanararat01@gmail.com>
2019-04-06 21:11:30 +0300

    Initial commit

---
+++ src/main/java/com/orhanararat/petclinicspringframework/PetClinicSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.orhanararat.petclinicspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicSpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicSpringFrameworkApplication.class, args);
+    }
+
+}
