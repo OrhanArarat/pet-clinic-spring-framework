85589fa5550a3bfe8ed3eedb20e5b22ac812e110
Orhan Ararat <orhanararat@Orhan-MacBook-Pro.local>
2019-04-25 19:28:44 +0300

    Adding bootstrap for data. Closes #12

---
+++ web/src/main/java/com/orhanararat/bootstrap/DataLoader.java
@@ -0,0 +1,66 @@
+package com.orhanararat.bootstrap;
+
+import com.orhanararat.model.Owner;
+import com.orhanararat.model.Vet;
+import com.orhanararat.services.OwnerService;
+import com.orhanararat.services.VetService;
+import com.orhanararat.services.map.OwnerServiceMap;
+import com.orhanararat.services.map.VetServiceMap;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    // == fields ==
+
+    private final OwnerService ownerService;
+
+    private final VetService vetService;
+
+    // == constructor ==
+
+    public DataLoader() {
+
+        ownerService = new OwnerServiceMap();
+
+        vetService = new VetServiceMap();
+
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded Owners............");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded Vets...............");
+    }
+}
