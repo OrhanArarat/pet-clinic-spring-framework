3a5c8a438e0faa13fa226a758c26ff2f9440536d
OrhanArarat <orhanararat01@gmail.com>
2019-06-15 18:14:56 +0300

    Implemented Base Entity ID value (JPA). Closes #32

--- core/src/main/java/com/orhanararat/model/BaseEntity.java
+++ core/src/main/java/com/orhanararat/model/BaseEntity.java
@@ -3,13 +3,20 @@ package com.orhanararat.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
     // == fields ==
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public boolean isNew() {
