e4c4c7634da08b0bc91ee8db9326a674e46063ec
OrhanArarat <orhanararat01@gmail.com>
2019-06-16 18:25:37 +0300

    Added Pet Spring Data Jpa Service. Closes #40

---
+++ core/src/main/java/com/orhanararat/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,51 @@
+package com.orhanararat.services.springdatajpa;
+
+import com.orhanararat.model.Pet;
+import com.orhanararat.repositories.PetRepository;
+import com.orhanararat.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService implements PetService {
+    // == fields ==
+    private final PetRepository petRepository;
+
+    // == constructor ==
+    public PetSDJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
