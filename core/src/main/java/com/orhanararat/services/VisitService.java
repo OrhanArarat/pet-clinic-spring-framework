02746af71a80e5612e2fff6281b6895459d5e5fd
OrhanArarat <orhanararat01@gmail.com>
2019-06-16 18:50:16 +0300

    Added VisitMapService class. Closes #42

---
+++ core/src/main/java/com/orhanararat/services/VisitService.java
@@ -0,0 +1,7 @@
+package com.orhanararat.services;
+
+import com.orhanararat.model.Visit;
+
+public interface VisitService extends CrudService<Visit,Long> {
+
+}
---
+++ core/src/main/java/com/orhanararat/services/map/VisitMapService.java
@@ -0,0 +1,33 @@
+package com.orhanararat.services.map;
+
+import com.orhanararat.model.Visit;
+import com.orhanararat.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstractMapService<Visit,Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
